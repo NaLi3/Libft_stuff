a38c1f13c25360d33046e950e7324bb8b4187a5e
NaLi3 <167555339+NaLi3@users.noreply.github.com>
2024-11-05 20:42:39 +0100

    A: First 8 c files
    Added the structure of the libft as well as the first 8 c files given in the subject.

---
+++ libft_stuff/c_files/ft_strlcpy.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilevy <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 20:06:31 by ilevy             #+#    #+#             */
+/*   Updated: 2024/11/05 20:38:04 by ilevy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	dest_length;
+	size_t	src_length;
+
+	src_length = ft_strlen(src);
+	dest_length = ft_strlen(dest);
+	j = dest_length;
+	i = 0;
+	if (dest_length < size - 1 && size > 0)
+	{
+		while (src[i] && dest_length + i < size - 1)
+		{
+			dest[j] = src[i];
+			j++;
+			i++;
+		}
+		dest[j] = 0;
+	}
+	if (dest_length >= size)
+		dest_length = size;
+	return (dest_length + src_length);
+}
